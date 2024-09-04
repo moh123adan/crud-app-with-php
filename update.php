8f95bd097593cc1cfafebea543aa9d61890370a6
maxamed <maxamedadan169@gmail.com>
2024-09-04 17:03:39 +0300

    second commit

--- update.php
+++ update.php
@@ -1,8 +1,18 @@
 <?php
-
-<?php
 include "header.php";
 include 'connection.php';
+
+
+$id = $_GET['updateid'];
+$sql = "select * from `crud` where id=$id";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_assoc($result);
+$name = $row['name'];
+$email = $row['email'];
+$phone = $row['phone'];
+$password = $row['password'];
+
+
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
@@ -12,11 +22,12 @@ if (isset($_POST['submit'])) {
 
 
 
-    $sql = "insert into `crud` (name, email, phone, password) values('$name', '$email', '$phone', '$password')";
+    $sql = "update `crud` set id=$id, name='$name', email='$email', phone='$email', password='$password' where id=$id";
     $result = mysqli_query($con, $sql);
 
     if ($result) {
-        header('location:display.php');
+        echo "updated succsfully";
+        // header('location:display.php');
     } else {
         die(mysqli_error($con));
     }
@@ -27,23 +38,23 @@ if (isset($_POST['submit'])) {
     <form method="post">
         <div class="form-group">
             <label>Name</label>
-            <input type="text" class="form-control" placeholder="Enter your name" name="name">
+            <input type="text" class="form-control" placeholder="Enter your name" name="name" value="<?php echo $name ?>">
         </div>
         <div class="form-group">
             <label>Email</label>
-            <input type="email" class="form-control" placeholder="Enter your email" name="email">
+            <input type="email" class="form-control" placeholder="Enter your email" name="email" value="<?php echo $email ?>">
         </div>
         <div class="form-group">
             <label>phone</label>
-            <input type="phone" class="form-control" placeholder="Enter your phone" name="phone">
+            <input type="phone" class="form-control" placeholder="Enter your phone" name="phone" value="<?php echo $phone ?>">
         </div>
         <div class="form-group">
             <label>password</label>
-            <input type="password" class="form-control" placeholder="Enter your password" name="password">
+            <input type="password" class="form-control" placeholder="Enter your password" name="password" value="<?php echo $password ?>">
         </div>
 
 
-        <button type="submit" class="btn btn-primary mt-4" name="submit">Submit</button>
+        <button type="submit" class="btn btn-primary mt-4" name="submit">Update</button>
 
     </form>
 </div>
@@ -51,7 +62,3 @@ if (isset($_POST['submit'])) {
 </body>
 
 </html>
-
-
-
-?>
